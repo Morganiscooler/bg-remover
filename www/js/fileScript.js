2c5075d8fdd5c2cd366ad9d45b6bafccd9806422
Morgan C <morgancuiuwu@gmail.com>
2025-04-05 14:16:51 -0400

    img preview works, background removing script not yet

--- www/js/fileScript.js
+++ www/js/fileScript.js
@@ -16,6 +16,29 @@ fileInput.addEventListener('change', function () {
 
 
 //   Displaying the image is bugged, fix this later.
-function displayImage(event){
-    document.getElementById("showImg").src = URL.createObjectURL(event.target.files[0]);
-}
+const previewContainer = document.getElementById("imagePreview");
+const previewImage = previewContainer.querySelector(".image-preview__image");
+const previewDefaultText = previewContainer.querySelector(".image-preview__default-text");
+
+fileInput.addEventListener("change", function(){
+  const file = this.files[0];
+
+  if (file){
+      const reader = new FileReader();
+
+      previewDefaultText.style.display = "none";
+      previewImage.style.display = "block";
+
+      reader.addEventListener("load", function(){
+        previewImage.setAttribute("src", this.result);
+      });
+
+      reader.readAsDataURL(file);
+
+    }
+    else{
+      previewDefaultText.style.display = null;
+      previewImage.style.display = null;
+      previewImage.setAttribute("src", "");
+    }
+})
